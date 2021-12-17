c1138e1b19e780885c50a6450d8db4203fe5d020
Jesus Cobarrubia <jesuscobarrubia18@gmail.com>
2021-12-17 00:17:18 -0300

    Creando primer componente

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import react from "react";
+
+const Navbar = () => {
+    return(
+        <>
+            <header>
+                <h1>Ecommerce</h1>
+
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Nosotros</a></li>
+                    <li><a href="#">Contacto</a></li>
+                </ul>
+            </header>
+        </>
+    )
+}
+
+export default Navbar;
